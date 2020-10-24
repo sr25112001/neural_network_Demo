898a96d8723ca3b50dafff23cec8b5275bdb8b43
shubham rajput <66172291+sr25112001@users.noreply.github.com>
2020-10-25 04:06:35 +0530

    Update basic_NN_code.py

--- basic_NN_code.py
+++ basic_NN_code.py
@@ -1,12 +1,12 @@
 # shubham
 import numpy as np
-import time 
+import time
 # np.random.seed(1)  #uncomment to disable randomization
 
-class Layer_Dense:  # layer obeject
-    def __init__(self, n_inputs, n_neurons):  # n_inputs = no. of features, n_neurons = no. of outputs
-        self.weights = 3 * np.random.randn(n_inputs, n_neurons)
-        self.biases = np.zeros((1, n_neurons))
+class Layer:  # layer obeject
+    def __init__(self, inputs, neurons):  # inputs = no. of features, neurons = no. of outputs
+        self.weights = 3 * np.random.randn(inputs, neurons)
+        self.biases = np.zeros((1, neurons))
 
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
@@ -31,20 +31,26 @@ training_inputs = np.array([[1, 1, 0],
                             [1, 0, 0]])
 
 training_outputs = np.array([[1, 0, 1, 0, 0, 1, 0, 1]]).T
-input_layer = Layer_Dense(3, 1)
+
+
+input_layer = Layer(3, 1)
 no_of_iterations = 1000  #more the no. of iterations more the accuracy
 pause_time = 1 #make this 0 for normal execution
+
 for iteration in range(no_of_iterations):
     input_layer.forward(training_inputs)
     outputs = sigmoid(input_layer.output)
+    
     error = training_outputs - outputs
     adjustments = error * d_sigmoid(outputs)
     input_layer.weights += np.dot(training_inputs.T, adjustments)
+    
     time.sleep(pause_time)
     #print('actual predicted outputs :', outputs.T)  #uncomment to see actual predicted values
     print('iteration no.',iteration+1)
     print('correct outputs : ', training_outputs.T)
     print('predicted outputs :', np.round(outputs).T)
+    
     if list(training_outputs) == list(np.round(outputs)):
         print('correct output achieved at',str(iteration+1)+'th iteration.')
         break
